28a4f38ba223fa50c97986c75a9191c9a6010f0c
Juan Hernandez <juanhernandez@Juans-iMac.local>
2019-06-02 09:07:23 -0500

    skeleton for step defs created

---
+++ atm/src/test/java/nicebank/RunCukesTest.java
@@ -0,0 +1,12 @@
+package nicebank;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = { "html:target/cucumber-html-report", "json:target/cucumber-json-report.json" }
+)
+public class RunCukesTest {
+}
