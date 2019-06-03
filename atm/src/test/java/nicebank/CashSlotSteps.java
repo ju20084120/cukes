edc1c601d585defe0baaf389ef475a5a663f4c93
Juan Hernandez <juanhernandez@Juans-iMac.local>
2019-06-03 15:20:37 -0500

    moved back AccountSteps to its own class

--- atm/src/test/java/nicebank/CashSlotSteps.java
+++ atm/src/test/java/nicebank/CashSlotSteps.java
@@ -20,23 +20,4 @@ public class CashSlotSteps {
         Assert.assertEquals("Incorrect amount of dispensed cash - ", amount,
                 helper.getCashSlot().getContents());
     }
-
-    public static class AccountSteps {
-        KnowsTheDomain helper;
-
-        public AccountSteps(KnowsTheDomain helper) {
-            this.helper = helper;
-        }
-
-        @Given("^I have deposited (\\$\\d+\\.\\d+) in my account$")
-        public void i_have_deposited_$_in_my_account(@Transform(MoneyConverter.class) Money amount) throws Throwable{
-            helper.getMyAccount().credit(amount);
-        }
-
-        @And("^the balance of my account should be (\\$\\d+\\.\\d+)$")
-        public void the_balance_of_my_account_should_be_$(@Transform(MoneyConverter.class) Money amount) throws Throwable{
-            Assert.assertEquals("Incorrect account balance - ", amount,
-                    helper.getMyAccount().getBalance());
-        }
-    }
 }
