b7301305f92cc2c1f8e76df45d952fe2bb5b5c57
Juan Hernandez <juanhernandez@Juans-iMac.local>
2019-06-04 10:38:02 -0500

    solved conflict between selenium dependency and jetty: jetty-repacked dep added to pom.xml

--- atm/src/main/java/nicebank/AtmUserInterface.java
+++ atm/src/main/java/nicebank/AtmUserInterface.java
@@ -1,8 +1,30 @@
 package nicebank;
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
 public class AtmUserInterface implements Teller {
 
+    private EventFiringWebDriver webDriver;
+
+    public AtmUserInterface(){
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver");
+        this.webDriver = new EventFiringWebDriver(new FirefoxDriver());
+
+    }
+
     public void withdrawFrom(Account account, int dollars) {
+
+        try {
+            webDriver.get("http://localhost:9988");
+            webDriver.findElement(By.id("Amount"))
+                    .sendKeys(String.valueOf(dollars));
+            webDriver.findElement(By.id("Withdraw")).click();
+        } finally {
+            webDriver.close();
+        }
 
     }
 }
